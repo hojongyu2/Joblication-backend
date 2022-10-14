b1ba709409eb5c221c33d7ccf52f37d54b58466c
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-10-14 01:02:56 -0400

    registration and login funcion complete

--- Src/app.js
+++ Src/app.js
@@ -1,26 +1,60 @@
-require('dotenv').config()
-const bodyParser = require('body-parser');
-const express = require('express');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
 const port = process.env.PORT;
-const mongoose = require('mongoose')
-const userRouter = require('./routes/userRouter')
-const cors = require('cors')
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRouter");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const UserModel = require('./models/UserModels');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
-
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
-.then(()=> console.log("Connected to mongo db successfully"))
-.catch(()=> console.log("unable to connect to mongodb from HJ"))
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_STRING)
+  .then(() => console.log("Connected to mongo db successfully"))
+  .catch(() => console.log("unable to connect to mongodb"));
 
 //cors
-app.use(cors());
+app.use(cors({
+    credentials:true,
+    origin: "http://localhost:3000",
+}));
+
+app.use(cookieParser());
 
 //parsing JSON to req.body
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.use(async (req, res, next) => {
+  try {
+    const { session_token: sessionToken } = req.cookies;
+
+    if (!sessionToken) {
+      return next();
+    }
+    const { userId, iat } = jwt.verify(
+      sessionToken,
+      process.env.AUTH_SECRET_KEY
+    );
+    if (iat < Date.now() - 30 * 24 * 60 * 60 * 1000) {
+      return res.status(401).json("session expired");
+    }
+    const foundUser = await UserModel.findOne({ _id: userId });
+
+    if (!foundUser) {
+      return next();
+    }
+    req.user = foundUser;
+
+    return next();
+    
+  } catch (error) {
+    next(error);
+  }
+});
 //userRoutes
-app.use(userRouter)
+app.use(userRouter);
 
-
-app.listen(port, () => console.log('joblication is working properly'))
+app.listen(port, () => console.log("joblication is working properly"));
--- Src/routes/userRouter.js
+++ Src/routes/userRouter.js
@@ -1,29 +1,77 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const UserModel = require("../models/UserModels");
+const jwt = require("jsonwebtoken");
+
+const cleanUser = (userDocument) => {
+    return {
+        id: userDocument._id,
+        firstName: userDocument.firstName,
+        lastName: userDocument.lastName,
+        email: userDocument.email,
+        jobTitle: userDocument.jobTitle,
+        isAdmin: userDocument.isAdmin,
+    };
+};
+
+const getToken = (userId) => {
+    return jwt.sign({ userId, iat: Date.now() }, process.env.AUTH_SECRET_KEY);
+};
+
 const userRouter = express.Router();
-const UserModel = require('../models/UserModels')
 
-userRouter.post('/register-user', async (req, res, next) => {
+userRouter.get("/sign-out", (req, res, next) => {
+  res.clearCookie("session_token");
+  res.send("Signed out successfully");
+});
 
-    const {firstName, lastName, email, password, jobTitle} = req.body
-    //console.log(req.body)
-    const hashedPassword = await bcrypt.hash(password, 5)
-    console.log(hashedPassword)
+userRouter.post("/register-user", async (req, res, next) => {
+  const { firstName, lastName, email, password, jobTitle } = req.body;
+  //console.log(req.body)
+  const hashedPassword = await bcrypt.hash(password, 5);
+  console.log(hashedPassword);
 
+  try {
     const userDocument = new UserModel({
-        firstName, lastName, email, hashedPassword, jobTitle
-    })
+      firstName,
+      lastName,
+      email,
+      hashedPassword,
+      jobTitle,
+    });
     userDocument.save();
 
-    res.json({ user : {
-        id:userDocument._id,
-        firstName, lastName, email, jobTitle,
-    }})
+    // const token = getToken(userDocument._id);
+    // res.cookie("session_token", token, { httpOnly: true, secure: false });
+    res.json({ user: cleanUser(userDocument) });
+  } catch (error) {
+    next(error);
+  }
+});
 
-})
+userRouter.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body.credentials;
 
+  try {
+    const foundUser = await UserModel.findOne({ email: email });
+    if (!foundUser) {
+      return res.status(401).send("user not found");
+    }
+    const passwrodMatch = await bcrypt.compare(
+      password,
+      foundUser.hashedPassword
+    );
+    // console.log(passwrodMatch)
+    if (!passwrodMatch) {
+      return res.status(401).send("wrong password");
+    }
 
+    const token = getToken(foundUser._id);
+    res.cookie("session_token", token, { httpOnly: true, secure: false });
+    res.json({ user: cleanUser(foundUser) });
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-
-module.exports = userRouter
+module.exports = userRouter;
